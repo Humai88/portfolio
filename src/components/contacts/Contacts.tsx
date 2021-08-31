8f3a52d8923b0315754acfebc15d7477c8cce32b
Gumay <humaivier88@gmail.com>
2021-08-31 21:07:19 +0100

    Add Formik

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form } from "./Form";
+import { ContactForm } from "./Form";
 import styles from "./Contacts.module.scss";
 import Button from "../../UI/Button/Button";
 
@@ -8,8 +8,7 @@ export const Contacts = () => {
     <div id="contacts" className={styles.wrapper}>
       <h2>Contact</h2>
       <div className={styles.line}></div>
-      <Form />
-      <Button className={styles.btn}>send message</Button>
+      <ContactForm />
     </div>
   );
 };
--- src/components/contacts/Form.tsx
+++ src/components/contacts/Form.tsx
@@ -1,13 +1,54 @@
-import React from "react";
-
+import { Formik, Form } from "formik";
+import Button from "../../UI/Button/Button";
+import * as Yup from "yup";
 import styles from "./Form.module.scss";
+import { Input } from "../../UI/Input/Input";
+import { Textarea } from "../../UI/Textarea/Textarea";
+import axios from "axios";
 
-export const Form = () => {
+export const ContactForm = () => {
   return (
-    <form action="" className={styles.form}>
-      <input placeholder="Name" />
-      <input placeholder="E-mail" />
-      <textarea name="" placeholder="Your message"></textarea>
-    </form>
+    <>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          message: "",
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string()
+            .min(2, "Must be at least 2 characters")
+            .max(15, "Must be 15 characters or less")
+            .required("Required!"),
+          email: Yup.string()
+            .email("Must be a valid email!")
+            .required("Required!"),
+          message: Yup.string().min(50, "Must be at least 50 characters"),
+        })}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          resetForm();
+          setSubmitting(false);
+          // console.log(values);
+          // axios
+          //   .post("/api/forma", values)
+          //   .then((res) => console.log(res))
+          //   .catch((err) => console.log("Message not sent"));
+        }}
+      >
+        {(props) => {
+          return (
+            <Form className={styles.form}>
+              <Input type="text" name="name" placeholder="Name" />
+              <Input type="text" name="email" placeholder="E-mail" />
+              <Textarea name="message" placeholder="Your message" />
+              <div className={styles.msg}>Message has been sent</div>
+              <Button className={styles.btn} type="submit">
+                {props.isSubmitting ? "Loading..." : "send message"}
+              </Button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </>
   );
 };
