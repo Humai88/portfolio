cabe839ef32c6c8bcf43b1286248b7b15de8893a
Gumay <humaivier88@gmail.com>
2021-08-31 16:12:01 +0100

    Add links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,9 +5,27 @@ export const Footer = () => {
       <div className={styles.container}>
         <h3>Gumay</h3>
         <div className={styles.links}>
-          <a href="l">Linkedin</a>
-          <a href="i">Instagram</a>
-          <a href="f">Facebook</a>
+          <a
+            href="https://www.linkedin.com/in/gumay-vierdiyeva-296949215/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://www.instagram.com/gumay88/?hl=en"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.facebook.com/gumay.verdiyeva/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Facebook
+          </a>
         </div>
         <p>Copyright</p>
       </div>
