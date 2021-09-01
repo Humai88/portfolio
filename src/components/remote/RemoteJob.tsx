17b4f4c0be50071e2aba890d0bfe136f2e0a6f30
Gumay <humaivier88@gmail.com>
2021-09-01 18:02:08 +0100

    Some corrections

--- src/components/remote/RemoteJob.tsx
+++ src/components/remote/RemoteJob.tsx
@@ -7,7 +7,13 @@ export const RemoteJob = () => {
     <div className={styles.wrapper}>
       <h3>I Am Available For Remote Job</h3>
       <div className={styles.line}></div>
-      <Button className={styles.btn}>Hire me</Button>
+      <a
+        href="https://www.linkedin.com/in/gumay-vierdiyeva-296949215/detail/contact-info/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Button className={styles.btn}>Hire me</Button>
+      </a>
     </div>
   );
 };
--- src/components/skills/SkillsSection.tsx
+++ src/components/skills/SkillsSection.tsx
@@ -19,22 +19,66 @@ export const SkillsSection = () => {
       <h2>My skills</h2>
       <div className={styles.line}></div>
       <div className={styles.container}>
-        <Skill img={<SiJavascript className={styles.icon} />} />
-        <Skill img={<SiTypescript className={styles.icon} />} />
-        <Skill img={<SiHtml5 className={styles.icon} />} />
-        <Skill img={<SiCss3 className={styles.icon} />} />
-        <Skill img={<SiReact className={styles.icon} />} />
-        <Skill img={<SiRedux className={styles.icon} />} />
-        <Skill img={<SiGit className={styles.icon} />} />
-        <Skill img={<FaSass className={styles.icon} />} />
+        <Skill
+          img={
+            <SiJavascript
+              style={{ color: "#ECD662" }}
+              className={styles.icon}
+            />
+          }
+        />
+
+        <Skill
+          img={
+            <SiTypescript
+              style={{ color: "#2978B5" }}
+              className={styles.icon}
+            />
+          }
+        />
+        <Skill
+          img={<SiHtml5 style={{ color: "#FF4C29" }} className={styles.icon} />}
+        />
+        <Skill
+          img={<SiCss3 style={{ color: "#3DB2FF" }} className={styles.icon} />}
+        />
+        <Skill
+          img={<SiReact style={{ color: "#3EDBF0" }} className={styles.icon} />}
+        />
+        <Skill
+          img={<SiRedux style={{ color: "#7952B3" }} className={styles.icon} />}
+        />
+        <Skill
+          img={<SiGit style={{ color: "#DD2C00" }} className={styles.icon} />}
+        />
+        <Skill
+          img={<FaSass style={{ color: "#E36BAE" }} className={styles.icon} />}
+        />
       </div>
       <h2 className={styles.tools}>Tools I Use</h2>
       <div className={styles.line}></div>
       <div className={`${styles.container} ${styles.toolsWrapper}`}>
-        <Skill img={<SiVisualstudiocode className={styles.icon} />} />
-        <Skill img={<SiPostman className={styles.icon} />} />
-        <Skill img={<SiStorybook className={styles.icon} />} />
-        <Skill img={<FaYarn className={styles.icon} />} />
+        <Skill
+          img={
+            <SiVisualstudiocode
+              style={{ color: "#2978B5" }}
+              className={styles.icon}
+            />
+          }
+        />
+        <Skill
+          img={
+            <SiPostman style={{ color: "#FF5722" }} className={styles.icon} />
+          }
+        />
+        <Skill
+          img={
+            <SiStorybook style={{ color: "#FD6F96" }} className={styles.icon} />
+          }
+        />
+        <Skill
+          img={<FaYarn style={{ color: "#548CA8" }} className={styles.icon} />}
+        />
       </div>
     </div>
   );
